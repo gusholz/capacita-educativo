958bcc6bceab85b0b8106db3d0b5ac2f4b392120
Gustavo Holzmann <gustavobarretohv@live.com>
2025-09-29 16:30:04 -0300

    build: `Xcode` project creation

---
+++ educativo/educativo/educativoApp.swift
@@ -0,0 +1,17 @@
+//
+//  educativoApp.swift
+//  educativo
+//
+//  Created by Gustavo Holzmann on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct educativoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
