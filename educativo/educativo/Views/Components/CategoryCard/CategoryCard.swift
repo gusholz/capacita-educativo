eb21d8f17625b482253f137018889e2979c5218f
Gustavo Holzmann <gustavobarretohv@live.com>
2025-09-30 09:58:21 -0300

    feat: add `CategoryCard` Component

---
+++ educativo/educativo/Views/Components/CategoryCard/CategoryCard.swift
@@ -0,0 +1,50 @@
+//
+//  CategoryCard.swift
+//  educativo
+//
+//  Created by Gustavo Holzmann on 30/09/25.
+//
+
+import SwiftUI
+
+struct CategoryCard: View {
+    var categoryTitle: String
+    var sfImageName: String
+    var foregroundColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Circle()
+                    .frame(width: 40, height: 40)
+                    .foregroundStyle(.white)
+                    .overlay(
+                        Image(systemName: sfImageName)
+                    )
+            }
+            
+            Spacer()
+            
+            HStack {
+                Text(categoryTitle)
+                    .font(.system(size: 22, weight: .bold))
+                Spacer()
+            }
+        }
+        .padding()
+        .frame(width: 164, height: 164)
+        .background(backgroundColor)
+        .cornerRadius(8)
+    }
+}
+
+#Preview {
+    CategoryCard(
+        categoryTitle: "Saúde",
+        sfImageName: "heart.fill",
+        foregroundColor: .black,
+        backgroundColor: .yellow
+    )
+}
